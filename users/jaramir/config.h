25502730dbf35298ddaa139f529fa6f0482fa803
Francesco Gigli <jaramir@gmail.com>
2018-08-08 21:38:31 +0100

    refactoring

--- users/jaramir/config.h
+++ users/jaramir/config.h
@@ -9,7 +9,7 @@
 #endif
 
 #define ESC_ARR LT(_ARROWS, KC_ESC)
-#define RSH_ENT RSFT_T(KC_ENT)
+#define RSH_ENT MT(MOD_LSFT, KC_ENT)
+#define LSH_SPC MT(MOD_LSFT, KC_SPC)
 #define GUI_SHA LGUI(LSFT(KC_A))
 #define SCR_CAP LGUI(LSFT(LCTL(KC_4)))
-#define LSH_SPC LSFT_T(KC_SPC)
