0977ad1d1039603deb0043cfe82d2798787e32ab
Francesco Gigli <jaramir@gmail.com>
2018-08-13 18:32:38 +0100

    1-up the arrow layer

--- users/jaramir/config.h
+++ users/jaramir/config.h
@@ -8,8 +8,32 @@
         ED_NOTE(_GS6),
 #endif
 
+// Tap Layer Arrows
 #define ESC_ARR LT(_ARROWS, KC_ESC)
-#define RSH_ENT MT(MOD_LSFT, KC_ENT)
+#define SCL_ARR LT(_ARROWS, KC_SCLN)
+#define KCI_ARR LT(_ARROWS, KC_I)
+
+// Mod Tap
+#define RSH_ENT MT(MOD_RSFT, KC_ENT)
 #define LSH_SPC MT(MOD_LSFT, KC_SPC)
+
+// Spectacle shortcuts
+#define SP_FULL LALT(LGUI(KC_F))
+#define SP_CENT LALT(LGUI(KC_C))
+
+#define SP_TOP LALT(LGUI(KC_UP))
+#define SP_BOTT LALT(LGUI(KC_DOWN))
+#define SP_LEFT LALT(LGUI(KC_LEFT))
+#define SP_RGHT LALT(LGUI(KC_RIGHT))
+
+#define SP_TLFT LCTL(LGUI(KC_LEFT))
+#define SP_TRGH LCTL(LGUI(KC_RIGHT))
+#define SP_BLFT LSFT(LCTL(LGUI(KC_LEFT)))
+#define SP_BRGH LSFT(LCTL(LGUI(KC_RIGHT)))
+
+#define SP_NTHR LALT(LCTL(KC_RIGHT))
+#define SP_PTHR LALT(LCTL(KC_LEFT))
+
+// Various shortcuts
 #define GUI_SHA LGUI(LSFT(KC_A))
 #define SCR_CAP LGUI(LSFT(LCTL(KC_4)))
